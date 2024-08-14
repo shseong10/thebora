4416aa88676c812ba762e16610b4bd187c6ff908
hyeoksujang <jhs0692@naver.com>
2024-08-14 12:23:10 +0900

    버그 수정

--- src/main/java/com/icia/guree/controller/InventoryController.java
+++ src/main/java/com/icia/guree/controller/InventoryController.java
@@ -49,6 +49,7 @@ public class InventoryController {
         }
         return "hotdeal/addItem";
     }
+
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/hotdeal/add_item")
     public String addItem(InventoryDto inventory, HttpSession session, RedirectAttributes rttr) {
@@ -91,159 +92,150 @@ public class InventoryController {
         List<InventoryDto> iList = iSer.getInventoryList(sDto.getPageNum());
 
         if (iList != null) {
-        System.out.println("iList:" + iList);
-        model.addAttribute("iList", iList);
-        model.addAttribute("keyWord", sDto.getKeyWord());
-        model.addAttribute("currentPage", sDto.getPageNum());
-        model.addAttribute("totalPages", totalPages);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("pageCount", BoardService.PAGECOUNT);
+            System.out.println("iList:" + iList);
+            model.addAttribute("iList", iList);
+            model.addAttribute("keyWord", sDto.getKeyWord());
+            model.addAttribute("currentPage", sDto.getPageNum());
+            model.addAttribute("totalPages", totalPages);
+            model.addAttribute("startPage", startPage);
+            model.addAttribute("endPage", endPage);
+            model.addAttribute("pageCount", BoardService.PAGECOUNT);
         }
 
         return "hotdeal/itemList";
+    }
+
+    //상품 상세
+    @GetMapping("/hotdeal/list/detail")
+    public String inventoryDetail(@RequestParam("sb_num") Integer sb_num, Model model) throws JsonProcessingException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            System.out.println("-----현재 페이지에 접속중인 유저-----");
+            ProfileFile profile = mSer.myPage(userDetails.getUsername());
+            model.addAttribute("profile", profile);
         }
 
-//상품 상세
-@GetMapping("/hotdeal/list/detail")
-public String inventoryDetail(@RequestParam("sb_num") Integer sb_num, Model model) throws JsonProcessingException {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
-        System.out.println("-----현재 페이지에 접속중인 유저-----");
-        ProfileFile profile = mSer.myPage(userDetails.getUsername());
-        model.addAttribute("profile", profile);
-    }
-
-    log.info("<<<<<<<sb_num=" + sb_num);
-    if (sb_num == null) {
-        return "redirect:/hotdeal/list";
-    }
-    InventoryDto inventory = iSer.getInventoryDetail(sb_num);
-    log.info("<<<<<<<<InventoryDto: {}", inventory);
-    log.info("<<<<<<<<InventoryFile: {}", inventory.getIfList());
-    if (inventory != null) {
-        model.addAttribute("json", new ObjectMapper().writeValueAsString(inventory));
-        model.addAttribute("inventory", inventory);
-        return "hotdeal/viewItem";
-    } else {
-        return "redirect:/hotdeal/list";
-    }
-}
-
-//상품 수정하기
-@PreAuthorize("isAuthenticated()")
-@GetMapping("/hotdeal/update_item")
-public String updateItem(@RequestParam("sb_num") Integer sb_num, Model model) throws JsonProcessingException {
-    log.info("{}번째 상품 수정 페이지로 이동", sb_num);
-    InventoryDto inventory = iSer.getInventoryDetail(sb_num);
-    List<CategoryDto> cList = iSer.getCategoryList();
-    if (cList != null) {
-        System.out.println("cList:" + cList);
-        model.addAttribute("cList", cList);
-    }
-    if (inventory != null) {
-        model.addAttribute("json", new ObjectMapper().writeValueAsString(inventory));
-        model.addAttribute("inventory", inventory);
-        return "hotdeal/updateItem";
-    } else {
-        return "redirect:/hotdeal/list";
-    }
-}
-
-@PreAuthorize("isAuthenticated()")
-@PostMapping("/hotdeal/update_item")
-public String updateItem(@RequestParam("sb_num") Integer sb_num, InventoryDto inventory, HttpSession session, RedirectAttributes rttr,
-                         HttpServletRequest request) {
-    log.info("상품 수정");
-
-    for (MultipartFile mf : inventory.getAttachments()) {
-        log.info("추가 업로드 파일: {}", mf.getOriginalFilename());
-    }
-
-    boolean result = iSer.updateItem(inventory, session);
-    if (result) {
-        rttr.addFlashAttribute("msg", "상품 수정 성공");
-        //선택한 파일 삭제
-        if (request.getParameterValues("deleteFile").length > 0) {
-            Map fMap = new HashMap();
-            for (String bf_orifilename : request.getParameterValues("deleteFile")) {
-                log.info("IController/선택한 파일 삭제>>>FileManager {}", bf_orifilename);
-                fMap.put("sb_num", sb_num);
-                fMap.put("bf_orifilename", bf_orifilename);
-                fm.deleteSelFmap(session, fMap);
-            }
+        log.info("<<<<<<<sb_num=" + sb_num);
+        if (sb_num == null) {
+            return "redirect:/hotdeal/list";
         }
-        return "redirect:/hotdeal/list";
-    } else {
-        rttr.addFlashAttribute("msg", "상품 수정 실패");
-        return "redirect:/hotdeal/update_Item";
-    }
-}
-
-//상품 삭제하기 *이미 주문되었거나 장바구니에 담긴 상품은 삭제 불가.
-@PreAuthorize("isAuthenticated()")
-@GetMapping("/hotdeal/delete_item")
-public String deleteItem(@RequestParam("sb_num") Integer sb_num, HttpSession session, RedirectAttributes rttr) {
-    log.info(">>>>>삭제 대상 글번호: {}", sb_num);
-
-    try {
-        iSer.deleteItem(sb_num, session); // *****
-        rttr.addFlashAttribute("msg", sb_num + "번 삭제성공");
-        return "redirect:/hotdeal/list";
-    } catch (DBException e) {
-        log.info(e.getMessage());
-        rttr.addFlashAttribute("msg", sb_num + "번 삭제실패");
-        return "redirect:/hotdeal/list/detail?sb_num=" + sb_num;
-    }
-}
-
-//관리자페이지
-@GetMapping("/hotdeal/admin/main")
-public String getAdmin(SearchDto sDto, Model model, HttpSession session) throws JsonProcessingException {
-    List<CategoryDto> cList = iSer.getCategoryList();
-    if (cList != null) {
-        System.out.println("cList:" + cList);
-        model.addAttribute("cList", cList);
+        InventoryDto inventory = iSer.getInventoryDetail(sb_num);
+        log.info("<<<<<<<<InventoryDto: {}", inventory);
+        log.info("<<<<<<<<InventoryFile: {}", inventory.getIfList());
+        if (inventory != null) {
+            model.addAttribute("json", new ObjectMapper().writeValueAsString(inventory));
+            model.addAttribute("inventory", inventory);
+            return "hotdeal/viewItem";
+        } else {
+            return "redirect:/hotdeal/list";
+        }
     }
 
-    int totalItems = iSer.countMarketItems(sDto);
-    int totalPages = (int) Math.ceil((double) totalItems / BoardService.LISTCNT);
-    if (sDto.getPageNum() == null || sDto.getPageNum() < 1) {
-        sDto.setPageNum(1);
-    } else if (sDto.getPageNum() > totalPages) {
-        sDto.setPageNum(totalPages);
+    //상품 수정하기
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/hotdeal/update_item")
+    public String updateItem(@RequestParam("sb_num") Integer sb_num, Model model) throws JsonProcessingException {
+        log.info("{}번째 상품 수정 페이지로 이동", sb_num);
+        InventoryDto inventory = iSer.getInventoryDetail(sb_num);
+        List<CategoryDto> cList = iSer.getCategoryList();
+        if (cList != null) {
+            System.out.println("cList:" + cList);
+            model.addAttribute("cList", cList);
+        }
+        if (inventory != null) {
+            model.addAttribute("json", new ObjectMapper().writeValueAsString(inventory));
+            model.addAttribute("inventory", inventory);
+            return "hotdeal/updateItem";
+        } else {
+            return "redirect:/hotdeal/list";
+        }
     }
 
-    int currentGroup = (sDto.getPageNum() - 1) / BoardService.PAGECOUNT;
-    int startPage = currentGroup * BoardService.PAGECOUNT + 1;
-    int endPage = Math.min(startPage + BoardService.PAGECOUNT - 1, totalPages);
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/hotdeal/update_item")
+    public String updateItem(InventoryDto inventory, HttpSession session, RedirectAttributes rttr) {
+        log.info("상품 수정");
 
-    List<InventoryDto> iList = iSer.getInventoryList(sDto.getPageNum());
+        for (MultipartFile mf : inventory.getAttachments()) {
+            log.info("추가 업로드 파일: {}", mf.getOriginalFilename());
+        }
 
-    if (iList != null) {
-        System.out.println("관리자페이지 테이블 출력==================");
-        System.out.println("iList:" + iList);
-        model.addAttribute("iList", iList);
-        model.addAttribute("json", new ObjectMapper().writeValueAsString(iList));
-        model.addAttribute("keyWord", sDto.getKeyWord());
-        model.addAttribute("currentPage", sDto.getPageNum());
-        model.addAttribute("totalPages", totalPages);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("pageCount", BoardService.PAGECOUNT);
-        return "hotdeal/adminMain";
+        boolean result = iSer.updateItem(inventory, session);
+        if (result) {
+            rttr.addFlashAttribute("msg", "상품 수정 성공");
+            //선택한 파일 삭제
+            return "redirect:/hotdeal/list";
+        } else {
 
-    } else {
-        return "redirect:/hotdeal/list";
+            rttr.addFlashAttribute("msg", "상품 수정 실패");
+            return "redirect:/hotdeal/update_Item";
+        }
     }
-}
 
-@ResponseBody
-@PostMapping("/upload/hotdeal")
-public Map<String, Object> editorFileUpload(MultipartHttpServletRequest request, HttpSession session) {
-    Map<String, Object> uploadedImg = fm.editorFileUpload(request, session);
-    return uploadedImg;
-}
+    //상품 삭제하기 *이미 주문되었거나 장바구니에 담긴 상품은 삭제 불가.
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/hotdeal/delete_item")
+    public String deleteItem(@RequestParam("sb_num") Integer sb_num, HttpSession session, RedirectAttributes rttr) {
+        log.info(">>>>>삭제 대상 글번호: {}", sb_num);
+
+        try {
+            iSer.deleteItem(sb_num, session); // *****
+            rttr.addFlashAttribute("msg", sb_num + "번 삭제성공");
+            return "redirect:/hotdeal/list";
+        } catch (DBException e) {
+            log.info(e.getMessage());
+            rttr.addFlashAttribute("msg", sb_num + "번 삭제실패");
+            return "redirect:/hotdeal/list/detail?sb_num=" + sb_num;
+        }
+    }
+
+    //관리자페이지
+    @GetMapping("/hotdeal/admin/main")
+    public String getAdmin(SearchDto sDto, Model model, HttpSession session) throws JsonProcessingException {
+        List<CategoryDto> cList = iSer.getCategoryList();
+        if (cList != null) {
+            System.out.println("cList:" + cList);
+            model.addAttribute("cList", cList);
+        }
+
+        int totalItems = iSer.countMarketItems(sDto);
+        int totalPages = (int) Math.ceil((double) totalItems / BoardService.LISTCNT);
+        if (sDto.getPageNum() == null || sDto.getPageNum() < 1) {
+            sDto.setPageNum(1);
+        } else if (sDto.getPageNum() > totalPages) {
+            sDto.setPageNum(totalPages);
+        }
+
+        int currentGroup = (sDto.getPageNum() - 1) / BoardService.PAGECOUNT;
+        int startPage = currentGroup * BoardService.PAGECOUNT + 1;
+        int endPage = Math.min(startPage + BoardService.PAGECOUNT - 1, totalPages);
+
+        List<InventoryDto> iList = iSer.getInventoryList(sDto.getPageNum());
+
+        if (iList != null) {
+            System.out.println("관리자페이지 테이블 출력==================");
+            System.out.println("iList:" + iList);
+            model.addAttribute("iList", iList);
+            model.addAttribute("json", new ObjectMapper().writeValueAsString(iList));
+            model.addAttribute("keyWord", sDto.getKeyWord());
+            model.addAttribute("currentPage", sDto.getPageNum());
+            model.addAttribute("totalPages", totalPages);
+            model.addAttribute("startPage", startPage);
+            model.addAttribute("endPage", endPage);
+            model.addAttribute("pageCount", BoardService.PAGECOUNT);
+            return "hotdeal/adminMain";
+
+        } else {
+            return "redirect:/hotdeal/list";
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("/upload/hotdeal")
+    public Map<String, Object> editorFileUpload(MultipartHttpServletRequest request, HttpSession session) {
+        Map<String, Object> uploadedImg = fm.editorFileUpload(request, session);
+        return uploadedImg;
+    }
 }
--- src/main/java/com/icia/guree/service/InventoryService.java
+++ src/main/java/com/icia/guree/service/InventoryService.java
@@ -27,13 +27,13 @@ public class InventoryService {
     @Autowired
     private FileManager fm;
 
-    public static final int LISTCNT=10; //페이지당 10개
-    public static final int PAGECOUNT=2; //페이지 그룹   이전 [3]  [4] 다음
+    public static final int LISTCNT = 10; //페이지당 10개
+    public static final int PAGECOUNT = 2; //페이지 그룹   이전 [3]  [4] 다음
 
     //상품 리스트 가져오기
     public List<InventoryDto> getInventoryList(Integer pageNum) {
-        int startIdx =(pageNum-1)*LISTCNT;
-        Map<String, Integer> pageMap=new HashMap<>();
+        int startIdx = (pageNum - 1) * LISTCNT;
+        Map<String, Integer> pageMap = new HashMap<>();
         pageMap.put("startIdx", startIdx);
         pageMap.put("listCnt", LISTCNT);
 
@@ -44,15 +44,15 @@ public class InventoryService {
     //상품 카테고리 가져오기
     public List<CategoryDto> getCategoryList() {
         List<CategoryDto> cList = null;
-        return  iDao.getCategoryList();
+        return iDao.getCategoryList();
     }
 
     //상품 등록하기
-    public boolean addItem (InventoryDto inventory, HttpSession session) {
+    public boolean addItem(InventoryDto inventory, HttpSession session) {
         boolean result = iDao.addItemSelectKey(inventory);
         if (result) {
-            if(!inventory.getAttachments().get(0).isEmpty()) {
-                if(fm.fileUpload(inventory.getAttachments(), session, inventory.getSb_num())) {
+            if (!inventory.getAttachments().get(0).isEmpty()) {
+                if (fm.fileUpload(inventory.getAttachments(), session, inventory.getSb_num())) {
                     log.info("=== 상품 등록 완료 ===");
                     return true;
                 }
@@ -69,25 +69,24 @@ public class InventoryService {
     }
 
     //상품 수정하기
-    public boolean updateItem (InventoryDto inventory, HttpSession session) {
+    public boolean updateItem(InventoryDto inventory, HttpSession session) {
         boolean result = iDao.updateItem(inventory);
         if (result) {
-            if(!inventory.getAttachments().get(0).isEmpty()) {
+            if (!inventory.getAttachments().get(0).isEmpty()) {
                 //기존 파일 삭제
-//                String[] sysFiles = iDao.getsysFiles(inventory.getSb_num());
-//                if(sysFiles.length != 0) {
-//                    fm.fileDelete(sysFiles, session);
-//                    iDao.deleteFile(inventory.getSb_num());
-//                }
-                if(fm.fileUpload(inventory.getAttachments(), session, inventory.getSb_num())) {
+                String[] sysFiles = iDao.getsysFiles(inventory.getSb_num());
+                if (sysFiles.length != 0) {
+                    fm.fileDelete(sysFiles, session);
+                    iDao.deleteFile(inventory.getSb_num());
+                }
+                if (fm.fileUpload(inventory.getAttachments(), session, inventory.getSb_num())) {
                     log.info("!!=== 상품 수정 완료 ===!!");
                     return true;
                 }
             }
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     //상품 삭제하기
@@ -95,19 +94,19 @@ public class InventoryService {
     public void deleteItem(Integer sb_num, HttpSession session) throws DBException {
         //파일 삭제
         String[] sysFiles = iDao.getsysFiles(sb_num);
-        if(sysFiles.length != 0) {
-            if(iDao.deleteFile(sb_num) == false) {
+        if (sysFiles.length != 0) {
+            if (iDao.deleteFile(sb_num) == false) {
                 log.info("!! deleteFile 예외발생");
                 throw new DBException(); //롤백
             }
         }
         //원글 삭제
-        if(!iDao.deleteItem(sb_num)) {
+        if (!iDao.deleteItem(sb_num)) {
             log.info("!! deleteItem 예외발생");
             throw new DBException(); //롤백
         }
         //서버 파일 삭제
-        if(sysFiles.length != 0) {
+        if (sysFiles.length != 0) {
             fm.fileDelete(sysFiles, session);
         }
     }
@@ -115,8 +114,8 @@ public class InventoryService {
     //관리자페이지
     //상품 리스트 가져오기
     public List<InventoryDto> getAdmin(Integer pageNum) {
-        int startIdx =(pageNum-1)*LISTCNT;
-        Map<String, Integer> pageMap=new HashMap<>();
+        int startIdx = (pageNum - 1) * LISTCNT;
+        Map<String, Integer> pageMap = new HashMap<>();
         pageMap.put("startIdx", startIdx);
         pageMap.put("listCnt", LISTCNT);
 
@@ -126,7 +125,7 @@ public class InventoryService {
 
     public List<InventoryDto> quickUpdate(InventoryDto inventory) {
         List<InventoryDto> iList = null;
-        if(iDao.quickUpdate(inventory)) {
+        if (iDao.quickUpdate(inventory)) {
 //            iList = iDao.getInventoryList();
             log.info("빠른 수정 저장 성공");
         }
